4395b211810dfce29fe104e1ed37211c299f6b4d
Shaurya <iiitianshauryashakya8055@gmail.com>
2025-08-21 20:43:46 +0530

    new

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 from fastapi import FastAPI, File, UploadFile
 from rembg import remove
-from fastapi.responses import Response
+from fastapi.responses import StreamingResponse
+import io
 
 app = FastAPI()
 
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
-    img = await file.read()
-    output = remove(img)
-    return Response(content=output, media_type="image/png")
+    input_image = await file.read()
+    output_image = remove(input_image)
+    return StreamingResponse(io.BytesIO(output_image), media_type="image/png")
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+#!/usr/bin/env bash
+exec uvicorn main:app --host 0.0.0.0 --port $PORT
